7acb2648f644ab645e51d3ba4c0bd0c02d0988ef
Hugo Moisés <hugo2018moises@gmail.com>
2026-09-16 15:28:23 -0300

    Adicionando página home e portfólio com fluxo de views/templates

--- my_app/views.py
+++ my_app/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hello, World!")
+    return render(request, "estudio/index.html")
+
+def portfolio(request):
+    return render(request, "estudio/portfolio.html")
